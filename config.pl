c15a9106a19acfdaaffa9c2f555bad4ec0c1ba8a
Fumihiro Itoh <fmhrit@gmail.com>
2013-01-12 00:47:47 +0900

    update config file

--- config.pl
+++ config.pl
@@ -6,5 +6,12 @@
     notifo_apisecret => '',
     hosts => {
         "localhost" => "127.0.0.1",
-    }
+    },
+    consumer_key => '',
+    consumer_secret => '',
+    authorize_uri => 'https://accounts.google.com/o/oauth2/auth',
+    access_token_uri => 'https://accounts.google.com/o/oauth2/token',
+    scope => 'https://spreadsheets.google.com/feeds https://docs.google.com/feeds',
+    redirect_uri => 'http://localhost:5000',
+    access_token_file => './access_token.pl',
 }

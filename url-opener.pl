3e22c9c0c0c8b6d03573b9a3a175457691fffc17
Fumihiro Itoh <fmhrit@gmail.com>
2013-01-16 00:31:30 +0900

    add Web Frontend to supervisor

--- url-opener.pl
+++ url-opener.pl
@@ -17,8 +17,13 @@ use Getopt::Long qw/:config posix_default no_ignore_case gnu_compat/;
 use URI;
 use Carp;
 use Proclet;
+use Plack::Loader;
+use Plack::Builder;
+use DBM::Deep;
 use App::Onion::Parser;
+use App::Onion::Web;
 
+# for debug
 use Data::Dumper;
 
 my $url = q#http://mixi.jp/#;
@@ -26,84 +31,138 @@ my $url = q#http://mixi.jp/#;
 my $debug;
 my $growl_notify;
 my $notify_to_phone; # 0 or "kayac" or "notifo"
-my $auto_url_open; # Denger!!
 my $auto_message_open;
 my $interval = 15;
 my $use_local_hosts;
 my $config_file = "./config.pl";
+my $port = 5000;
+my $host = 0;
+my $no_web;
+my $no_watcher;
 
 GetOptions(
-    debug => \$debug,
-    growl => \$growl_notify,
+    "d|debug" => \$debug,
+    "g|growl" => \$growl_notify,
+    "m|message_open" => \$auto_message_open,
+    "i|interval=s" => \$interval,
+    "p|port=s" => \$port,
+    "host=s" => \$host,
     "notify=s" => \$notify_to_phone,
-    message_open => \$auto_message_open,
-    "interval=s" => \$interval,
     "config=s" => \$config_file,
-    hosts => \$use_local_hosts,
+    "hosts" => \$use_local_hosts,
+    "no-web" => \$no_web,
+    "no-watcher" => \$no_watcher,
 );
 
 my $conf = do $config_file or die;
+
+# open databse file
+my $messages_detail = DBM::Deep->new(
+    file => "message.db",
+    type => DBM::Deep->TYPE_HASH,
+    autoflush => 1,
+);
+
+my $message_ids = DBM::Deep->new(
+    file => "message_list.db",
+    type => DBM::Deep->TYPE_ARRAY,
+    autoflush => 1,
+);
 
 my $uri_object = URI->new($url);
 
 my $mech = WWW::Mechanize->new();
 my ($res, $tree);
 
-$res = _mechanize_get($mech, $uri_object, "list_message.pl");
-# login
-$res = $mech->submit_form(
-    form_number => 1,
-    fields => {
-        email => $conf->{login_mail},
-        password => $conf->{login_password},
-    }
-);
-$res = $mech->follow_meta_redirect;
-
-my @message_ids = get_message_ids($res->decoded_content);
-
 my $cv = AnyEvent->condvar;
 
-# add watcher to supervisor
+# setup supervisor
 my $proclet = Proclet->new(color => 1);
 
-$proclet->service(
-    code => sub {
-        warn "start timer";
-        my $timer = AnyEvent->timer(
-            interval => $interval,
-            cb => \&timer_callback,
-        );
+# add watcher to supervisor
+unless ($no_watcher) {
+    $res = _mechanize_get($mech, $uri_object, "list_message.pl");
+    # login to web site
+    $res = $mech->submit_form(
+        form_number => 1,
+        fields => {
+            email => $conf->{login_mail},
+            password => $conf->{login_password},
+        }
+    );
+    $res = $mech->follow_meta_redirect;
 
-        warn $cv->recv;
+    {
+        my @new_message_ids = get_message_ids($res->decoded_content);
+        my @Ronly = calc_new_message_ids($message_ids, \@new_message_ids);
 
-        die;
-    },
-    worker => 1,
-    tag => "Watcher",
-);
+        if (scalar @Ronly > 0) {
+            store_new_messages(@Ronly);
+        }
+    }
+
+    $proclet->service(
+        code => sub {
+            say "Start timer";
+            my $timer = AnyEvent->timer(
+                interval => $interval,
+                cb => \&timer_callback,
+            );
+
+            warn $cv->recv;
+
+            die;
+        },
+        worker => 1,
+        tag => "Watcher",
+    );
+}
+
+# add web front end to supervisor
+unless ($no_web) {
+    $proclet->service(
+        code => sub {
+            my $frontend_app = builder {
+                enable "Plack::Middleware::Static",
+                    path => qr{^/(css|js|img)/},
+                    root => File::Spec->catdir(dirname(__FILE__), "static");
+                App::Onion::Web->to_app($conf);
+            };
+
+            # start app server
+            my $loader = Plack::Loader->load(
+                'Starlet',
+                port => $port,
+                host => $host || 0,
+                max_workers => 1,
+            );
+            say "Start Web Frontend";
+            say "Accepting connections at http://$host:$port/";
+            $loader->run($frontend_app);
+        },
+        worker => 1,
+        tag => "Web Frontend",
+    );
+}
 
 $proclet->run;
 
 sub timer_callback {
+    my $tree;
     my $res = _mechanize_get($mech, $uri_object, "list_message.pl");
     unless ($res) {
         $cv->send("Connection Lost!!");
         return;
     }
-    my $tree = HTML::TreeBuilder::XPath->new_from_content($res->decoded_content);
+    my @new_message_ids = get_message_ids($res->decoded_content);
 
-    my @new_messages = $tree->findvalues(q{//td[@class='subject']/a/@href});
-    my @new_message_ids = map { $_ =~ m/id=([0-9a-f]{32})/; $1} @new_messages;
-
-    my $lc = List::Compare->new(\@message_ids, \@new_message_ids);
-
-    my @Ronly = $lc->get_Ronly;
+    my @Ronly = calc_new_message_ids(\@$message_ids, \@new_message_ids);
 
     # 新規メッセージがないなら終了
     return if scalar @Ronly == 0;
+    # 以下新規メッセージがある場合の処理
 
-    push @message_ids, @Ronly;
+    store_new_messages(@Ronly);
 
     # notification
     if ($growl_notify) {
@@ -195,6 +254,47 @@ sub _mechanize_get {
     return $mech->get($uri->as_string);
 }
 
+sub calc_new_message_ids {
+    my $message_ids = shift;
+    my $new_message_ids = shift;
+
+    if (ref $message_ids eq "DBM::Deep::Array") {
+        $message_ids = dbm_array_to_array_ref($message_ids);
+    }
+
+    my $lc = List::Compare->new($message_ids, $new_message_ids);
+    my @Ronly = $lc->get_Ronly;
+
+    return wantarray ? @Ronly : \@Ronly;
+}
+
+sub dbm_array_to_array_ref {
+    my $dbm_array = shift;
+
+    my @normal_array = @$dbm_array;
+
+    return \@normal_array;
+}
+
+sub store_new_messages {
+    my @new_message_ids = @_;
+
+    # add message list db
+    push @$message_ids, @new_message_ids;
+
+    # initialize entry in message detail db
+    foreach my $message_id (@new_message_ids) {
+        $messages_detail->{$message_id} = {
+            sender => "",
+            sender_name => "",
+            send_date => "",
+            title => "",
+            body => "",
+            mark => 0,
+        };
+    }
+}
+
 __END__
 =pod
 
@@ -225,13 +325,13 @@ url-opener.pl - 監視スクリプト
 
 =over
 
-=item growl
+=item --growl -g
 
 新しいメッセージがあったらGrowl通知をします。
 
 growlnotifyコマンドがパスが通ったディレクトリにインストールされている必要があります。
 
-=item notify
+=item --notify -n
 
 通知をスマートフォンに送ります．growlフラグとは関係ありません．
 
@@ -250,9 +350,9 @@ Androidへ通知を送る事も出来ます．
 
 いずれのサービスを使う場合でもそれぞれのサイトで登録をしAPIを使える状態にする必要があります．
 
-http://im.kayac.com/
+L<http://im.kayac.com/>
 
-http://notifo.com/
+L<http://notifo.com/>
 
 im.kayac.comの場合は登録したユーザー名をconfigファイルへ記述，
 notifoの場合はユーザー名とAPI Secretをconfigファイルへ書く必要があります．
@@ -260,25 +360,32 @@ notifoの場合はユーザー名とAPI Secretをconfigファイルへ書く必�
 im.kayac.comは同時に複数のデバイスでSubscribeした場合は，後にログインした方のみに通知が飛びます．
 notifoは複数のデバイスでSubscribeした場合でもそれぞれのデバイスへ通知が飛びます．
 
-=item message_open
+=item --message_open -m
 
 自動で新着メッセージを開きます．自動でメッセージ内のURLを開くのが怖い人はこちらがオススメ．
 
-=item host
+=item hosts
 
 configファイル内に書かれたドメインとIPの対応を使用します．
 管理者権限がなく，hostsファイルが書き換えられない場合でもアクセスする事が出来るようになります．
 
-=item auto_url_open
+=item port
 
-B<非常に危険なオプションなのでプログラム内にハードコーディングされています>
+WebフロントエンドがListenするポート番号を指定します．
+デフォルトはB<8800>です．
 
-メッセージ内にURLが含まれていた場合は自動で開きます．
+=item host
 
-メッセージ内に5個以上URLが含まれていた場合は開きません．
+WebフロントエンドがListenするhost名を指定します．
+特に理由がない限り指定する必要はありません．
 
-system関数からopenコマンドを実行しています．
-標準のブラウザとして指定されているもので開きます．（特にアプリケーションの指定なし）
+=item no-watcher
+
+監視スクリプトは起動しない．
+
+=item no-web
+
+Webフロントエンドは起動しない．
 
 =back
 
